15387202a8986f8224b07aa70297116280bf41a2
kateMoscoso <katiemscs@gmail.com>
2019-12-25 18:42:47 +0100

    basic examples

--- README.md
+++ README.md
@@ -8,9 +8,19 @@ php -S localhost:8000 server.php
 ```
 
 ```
+php -S localhost:8000 router.php
+```
+
+# Make petitions
+
+```
 curl http://localhost:8000?resource_type=books -v
 ```
 
 ```
 curl http://localhost:8000?resource_type=books | jq
+```
+
+
+```curl -X 'POST' http://localhost:8000/books -d '{"title":" Harry Potter and the Goblet of Fire","id_author":1,"id_genre":2}' | jq
 ```
--- server.php
+++ server.php
@@ -14,27 +14,47 @@ if(!in_array($resourceType,$allowedResourcesTypes )){
 
 $books = [
     1=> [
-        'titulo' => 'Harry Potter and the Philosopher\'s Stone',
+        'title' => 'Harry Potter and the Philosopher\'s Stone',
         'id_author' => 1,
-        'i_genero' =>1
+        'id_genre' =>1
     ],
     2=> [
-        'titulo' => 'Harry Potter and the Chamber of Secrets',
+        'title' => 'Harry Potter and the Chamber of Secrets',
         'id_author' => 1,
-        'i_genero' =>1
+        'id_genre' =>1
     ]
-    ];
+];
 
-    header('Content-Type: application/json');
+header('Content-Type: application/json');
+$resourceId = array_key_exists('resource_id', $_GET) ? $_GET['resource_id']:'';
 
 switch(strtoupper($_SERVER['REQUEST_METHOD'])){
     case 'GET':
+        if(empty($resourceId)){
         echo json_encode($books);
+        }
+        else {
+            if( array_key_exists( $resourceId, $books) ){
+                echo json_encode( $books[ $resourceId ] );
+            }
+        }
     break;
     case 'POST':
+        $json = file_get_contents('php://input');
+        $books[] = json_decode($json, true);
+        echo array_keys($books)[count($books)-1];
     break;
     case 'DELETE':
+        if(!empty($resourceId) && array_key_exists( $resourceId, $books) ){
+            unset ($books[$resourceId]);
+            echo json_encode($books);
+        }
     break;
     case 'PUT':
+        if(!empty($resourceId) && array_key_exists( $resourceId, $books) ){
+            $json = file_get_contents('php://input');
+            $books[$resourceId] = json_decode($json, true);
+            echo json_encode($books);
+        }
     break;
 }
